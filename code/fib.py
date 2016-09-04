61c3ccd8fe9f5298152cdb057dea71576a82e014
lokiworks <lokiworks.yen@gmail.com>
2016-09-05 06:35:18 +0800

    fasdf

--- code/fib.py
+++ code/fib.py
@@ -1,4 +1,5 @@
 # https://github.com/beniz/fiboheap/blob/master/fiboheap.h
+import  math
 class fibnode:
     def __init__(self, k, p):
         self.mark = False
@@ -15,29 +16,123 @@ class fibheap:
         self.n = 0
         self.min = None
 
+    def iterate(self, head):
+        node = stop = head
+        flag = False
+        while True:
+            if node == stop and flag:
+                break
+            elif node == stop:
+                flag = True
+            yield  node
+            node = node.right
+
     def insert(self, x):
         x.degree = 0
         x.p = None
         x.child = None
         x.mark = False
-        if not min:
+        if not self.min:
             x.left = x
             x.right = x
-            min = x
+            self.min = x
         else:
             self.min.left.right = x
-            x.left = min.left
+            x.left = self.min.left
             self.min.left = x
             x.right = self.min
             if x.key < min.key:
-                min = x
+                self.min = x
         self.n = self.n + 1
+
     def minimum(self):
         return  self.min
 
+    def heaplink(self, y, x):
+        y.left.right = y.left
+        y.right.left = y.right
+        if x.child:
+            x.child.left.right = y
+            y.left = x.child.left
+            x.child.left = y
+            y.right = x.child
+        else:
+            x.child = y
+            y.right = y
+            y.left = y
+        y.p = x
+        x.degree = x.degree + 1
+        y.mark = False
+
+    def consolidate(self):
+        max_degree = math.log(self.n)/math.log((1+math.sqrt(5))/2)
+        A = [None]*(max_degree+2)
+
+        w = self.min
+        rootsize = 0
+        next = w
+        rootList = []
+        while True:
+            rootsize = rootsize + 1
+            rootList.append(next)
+            next = next.right
+            if next == w:
+                break
+
+        for i in range(0, rootsize):
+            w = rootList[i]
+            x = w
+            d = x.degree
+            while A[d]:
+                y = A[d]
+                if x.key > y.key:
+                    A[x], A[y] = A[y], A[x]
+                self.heaplink(y,x)
+                A[d] = None
+                d = d + 1
+            A[d] = x
+        self.min = None
+        for i in xrange(0, max_degree+2):
+            if A[i]:
+                if not min:
+                    self.min = A[i].left = A[i].right = A[i]
+                else:
+                    min.left.right = A[i]
+                    A[i].left = self.min
+
+
+
+
+
+
+
+    def extractMin(self):
+        z = self.min
+        if z:
+            x = z.child
+            if x:
+                children = [x for x in self.iterate(z.child)]
+                for i in xrange(0, len(children)):
+                    x = children[i]
+                    self.min.left.right = x
+                    x.left = self.min.left
+                    self.min.left = x
+                    x.right = self.min
+                    x.p = None
+            z.left.right = z.right
+            z.right.left = z.left
+            if z == z.right:
+                self.min = None
+            else:
+                self.min = z.right
+
+
+
+
+
     @staticmethod
     def union_fiheap(H1, H2):
-        H = fibheap();
+        H = fibheap()
         H.min = H1.min
         if H.min and H2.min:
             H.min.right.left = H2.min.left
